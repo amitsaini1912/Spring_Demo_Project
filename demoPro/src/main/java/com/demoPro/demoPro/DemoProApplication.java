cd32e8099965c6ea9e5dd47b86fe052246951658
amitsaini1912 <amitkumarsainig9@gmail.com>
2026-01-11 17:29:04 +0530

    Project SetUp

---
+++ demoPro/src/main/java/com/demoPro/demoPro/DemoProApplication.java
@@ -0,0 +1,13 @@
+package com.demoPro.demoPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProApplication.class, args);
+	}
+
+}
---
+++ demoPro/src/test/java/com/demoPro/demoPro/DemoProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoPro.demoPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
