a85fd94bfd38d9c864dbe6a897188a128158f81d
amitsaini1912 <amitkumarsainig9@gmail.com>
2026-01-11 17:38:45 +0530

    Bean and dependency injection first Setup

---
+++ demoPro/src/main/java/com/demoPro/demoPro/Apple.java
@@ -0,0 +1,10 @@
+package com.demoPro.demoPro;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+    public void eatApple(){
+        System.out.println("Hey there!! i'm Apple.");
+    }
+}
--- demoPro/src/main/java/com/demoPro/demoPro/DemoProApplication.java
+++ demoPro/src/main/java/com/demoPro/demoPro/DemoProApplication.java
@@ -1,13 +1,22 @@
 package com.demoPro.demoPro;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoProApplication{
+public class DemoProApplication implements CommandLineRunner {
+
+	@Autowired
+	Apple obj;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		obj.eatApple();
+	}
 }
